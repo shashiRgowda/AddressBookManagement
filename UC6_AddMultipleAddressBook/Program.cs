1f1d7ff5e88fe7c41cea8c252bf74a796e8025ca
shashiRgowda <shashikumartr64@gmail.com>
2021-06-01 20:26:19 +0530

    Added:UC6_AddMultipleAddressBook

---
+++ UC6_AddMultipleAddressBook/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=================================\nChoose the operation:\n1.Add Another Contact.\n2.View Saved Contact.\n3.Edit Existing Contact.\n4.Delete Existing Contact.\n5.Exit Adress Book Application.\n=================================");
+            var userInput = Console.ReadLine();
+            var addressBook = new AddressBookMain();
+
+            while (true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        addressBook.StoreContact();
+                        break;
+                    case "2":
+                        addressBook.DisplayAllContact();
+                        break;
+                    case "3":
+                        addressBook.EditContact();
+                        break;
+                    case "4":
+                        addressBook.DeleteContact();
+                        break;
+                    case "5":
+                        return;
+                    default:
+                        Console.WriteLine("Choose valid operation.");
+                        break;
+
+                }
+                Console.WriteLine("=================================\nChoose the operation:\n1.Add Another Contact.\n2.View Saved Contact.\n3.Edit Existing Contact.\n4.Delete Existing Contact.\n5.Exit Adress Book Application.\n=================================");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+
+}
