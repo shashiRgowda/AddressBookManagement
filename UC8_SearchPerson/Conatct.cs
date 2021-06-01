b665d8f62f8e6392bcf186c978326d6c5f68f106
shashiRgowda <shashikumartr64@gmail.com>
2021-06-01 20:30:33 +0530

    Added:UC8_SearchPerson

---
+++ UC8_SearchPerson/Conatct.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class Contact
+    {
+        //
+        public Contact(string firstname, string lastname, string address, string city, string state, string zipcode, string phonenumber, string email)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Address = address;
+            City = city;
+            State = state;
+            ZipCode = zipcode;
+            PhoneNumber = phonenumber;
+            Email = email;
+        }
+        public string FirstName;
+        public string LastName;
+        public string Address;
+        public string City;
+        public string State;
+        public string ZipCode;
+        public string PhoneNumber;
+        public string Email;
+
+    }
+}
---
+++ UC8_SearchPerson/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose the operation::\n2.View Saved Contacts.\n3.Edit Existing Contact.\n4.Delete Existing Contact.\n5.Search Person via City.\n6.Exit Adress Book Application.\n=================================");
+            var userInput = Console.ReadLine();
+            var addressBook = new AddressBookMain();
+
+            while (true)
+            {
+                switch (userInput)
+                {
+                    case "1":
+                        addressBook.StoreContact();
+                        break;
+                    case "2":
+                        addressBook.DisplayAllContact();
+                        break;
+                    case "3":
+                        addressBook.EditContact();
+                        break;
+                    case "4":
+                        addressBook.DeleteContact();
+                        break;
+                    case "5":
+                        addressBook.DuplicateChecker();
+                        break;
+                    case "7":
+                        addressBook.SearchPerson();
+                        break;
+                    case "6":
+                        return;
+                    default:
+                        Console.WriteLine("Choose valid operation.");
+                        break;
+
+                }
+                Console.WriteLine("Choose the operation::\n2.View Saved Contacts.\n3.Edit Existing Contact.\n4.Delete Existing Contact.\n5.Search Person via City.\n6.Exit Adress Book Application.\n=================================");
+                userInput = Console.ReadLine();
+            }
+        }
+    }
+
+}
